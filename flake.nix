36bcdf2af2c5137661b02f79ab34cd4b4c36b13f
r-vdp <ramses@well-founded.dev>
2024-02-17 18:06:11 +0100

    Include aarch64-linux builds

--- flake.nix
+++ flake.nix
@@ -1,26 +1,37 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }: {
-    legacyPackages.x86_64-linux.holo-dev-server-bin =
-      let
-        inherit (nixpkgs) lib;
-        system = "x86_64-linux";
-        pkgs = nixpkgs.legacyPackages.${system};
-        src = (import ./sources.nix).${system}.holo-dev-server-bin;
+  outputs = { self, nixpkgs }:
+    let
+      inherit (nixpkgs) lib;
+      forEachSystem = lib.genAttrs [
+        "x86_64-linux"
+      ];
+    in
+    {
+      legacyPackages = forEachSystem (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          srcs = (import ./sources.nix).${system};
+        in
+        {
+          holo-dev-server-bin =
+            let
+              src = srcs.holo-dev-server-bin;
 
-        runtimeDeps = lib.concatMapStringsSep " "
-          (dep:
-            builtins.appendContext dep.outPath {
-              ${dep.drvPath} = { outputs = [ "${dep.output}" ]; };
-            }
-          )
-          (lib.importJSON "${src}/depinfo.json");
-      in
-      pkgs.runCommand "holo-dev-server-bin" { } ''
-        mkdir -p $out/bin
-        echo ${runtimeDeps} > $out/.runtimedeps
-        cp -a ${src}/bin/holo-dev-server $out/bin/holo-dev-server
-      '';
-  };
+              runtimeDeps = lib.concatMapStringsSep " "
+                (dep:
+                  builtins.appendContext dep.outPath {
+                    ${dep.drvPath} = { outputs = [ "${dep.output}" ]; };
+                  }
+                )
+                (lib.importJSON "${src}/depinfo.json");
+            in
+            pkgs.runCommand "holo-dev-server-bin" { } ''
+              mkdir -p $out/bin
+              echo ${runtimeDeps} > $out/.runtimedeps
+              cp -a ${src}/bin/holo-dev-server $out/bin/holo-dev-server
+            '';
+        });
+    };
 }
