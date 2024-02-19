04de6a440c0f926a1fd59b4df376ea3c3aa19af7
r-vdp <ramses@well-founded.dev>
2024-02-19 22:59:11 +0100

    Get nixpkgs info from the passthru

--- flake.nix
+++ flake.nix
@@ -1,56 +1,56 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
-  outputs = { self, nixpkgs }:
+  outputs = { self }:
     let
-      inherit (nixpkgs) lib;
-      forEachSystem = lib.genAttrs [
+
+      nameValuePair =
+        name:
+        value:
+        { inherit name value; };
+
+      genAttrs =
+        names:
+        f:
+        builtins.listToAttrs (map (n: nameValuePair n (f n)) names);
+
+      srcs = import ./sources.nix;
+      forEachSystem = genAttrs [
         "aarch64-linux"
         "x86_64-linux"
       ];
       # We only run tests for x86_64-linux in CI
-      forEachTestSystem = lib.genAttrs [
+      forEachTestSystem = genAttrs [
         "x86_64-linux"
       ];
     in
     {
-      packages = forEachSystem (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-          srcs = (import ./sources.nix).${system};
-        in
-        {
-          holo-dev-server-bin =
-            let
-              src = srcs.holo-dev-server-bin;
-
-              # depinfo.json contains the info on the runtime dependencies of
-              # the original holo-dev-server binary, as detected by nix.
-              # Nix detects runtime dependencies by scanning for the
-              # out path hashes of the build-time inputs.
-              # So in order for nix to detect the right runtime dependencies,
-              # we create a string with the needed derivations in its string
-              # context and use that string in the -bin derivation.
-              # This will cause these derivations to be added to the build-time
-              # dependencies, which will in turn cause nix to scan for them in
-              # our output.
-              # NOTE: these dependencies are not vendored! So users need to be
-              # able to either build or substitute them.
-              runtimeDeps = lib.concatMapStringsSep " "
-                (dep:
-                  builtins.appendContext "" {
-                    ${dep.drvPath} = { outputs = [ "${dep.output}" ]; };
-                  }
-                )
-                (lib.importJSON "${src}/depinfo.json");
-            in
-            pkgs.runCommand "holo-dev-server-bin" { } ''
-              mkdir -p $out/bin
-              # this comment pulls in the build-time dependencies: ${runtimeDeps}
-              # If they appear in the binary, they will be registered as runtime dependencies.
-              cp -a ${src}/bin/holo-dev-server $out/bin/holo-dev-server
-            '';
-        });
+      packages = forEachSystem
+        (system:
+          let
+            pkgs = import srcs.nixpkgs { inherit system; };
+            inherit (pkgs) lib;
+          in
+          {
+            holo-dev-server-bin =
+              let
+                src = srcs.${system}.holo-dev-server-bin;
+              in
+              pkgs.runCommand "holo-dev-server-bin"
+                {
+                  # depinfo.json contains the build-time dependencies of
+                  # the original holo-dev-server binary.
+                  # Nix detects runtime dependencies by scanning for the
+                  # out path hashes of the build-time inputs.
+                  # NOTE: these dependencies are not vendored! So users need to be
+                  # able to either build or substitute them.
+                  buildInputs = map
+                    (inputAttr: pkgs.${inputAttr})
+                    (lib.importJSON "${src}/depinfo.json");
+                }
+                ''
+                  mkdir -p $out/bin
+                  cp -a ${src}/bin/holo-dev-server $out/bin/holo-dev-server
+                '';
+          });
 
       checks = forEachTestSystem (system: {
         holo-dev-server-bin = self.packages.${system}.holo-dev-server-bin;
