4e80122c450f2ec1e6ab8ceb6b8679eec8d022e9
r-vdp <ramses@well-founded.dev>
2024-02-19 13:19:17 +0100

    More documentation and cleanup

--- flake.nix
+++ flake.nix
@@ -5,11 +5,16 @@
     let
       inherit (nixpkgs) lib;
       forEachSystem = lib.genAttrs [
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+      # We only run tests for x86_64-linux in CI
+      forEachTestSystem = lib.genAttrs [
         "x86_64-linux"
       ];
     in
     {
-      legacyPackages = forEachSystem (system:
+      packages = forEachSystem (system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
           srcs = (import ./sources.nix).${system};
@@ -19,6 +24,18 @@
             let
               src = srcs.holo-dev-server-bin;
 
+              # depinfo.json contains the info on the runtime dependencies of
+              # the original holo-dev-server binary, as detected by nix.
+              # Nix detects runtime dependencies by scanning for the
+              # out path hashes of the build-time inputs.
+              # So in order for nix to detect the right runtime dependencies,
+              # we create a string with the needed derivations in its string
+              # context and use that string in the -bin derivation.
+              # This will cause these derivations to be added to the build-time
+              # dependencies, which will in turn cause nix to scan for them in
+              # our output.
+              # NOTE: these dependencies are not vendored! So users need to be
+              # able to either build or substitute them.
               runtimeDeps = lib.concatMapStringsSep " "
                 (dep:
                   builtins.appendContext "" {
@@ -29,9 +46,14 @@
             in
             pkgs.runCommand "holo-dev-server-bin" { } ''
               mkdir -p $out/bin
-              echo ${runtimeDeps} > $out/.runtimedeps
+              # this comment pulls in the build-time dependencies: ${runtimeDeps}
+              # If they appear in the binary, they will be registered as runtime dependencies.
               cp -a ${src}/bin/holo-dev-server $out/bin/holo-dev-server
             '';
         });
+
+      checks = forEachTestSystem (system: {
+        holo-dev-server-bin = self.packages.${system}.holo-dev-server-bin;
+      });
     };
 }
